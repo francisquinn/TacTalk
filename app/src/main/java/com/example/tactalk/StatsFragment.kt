e2f26cbb394bce429b51101d38fb6342ace30883
francisquinn <francisquinnn@gmail.com>
2020-12-08 20:23:09 +0000

    Pushing Josh's template code
    - Template pages
    - Recording page, end game page, stats page
    - Updated strings.xml

---
+++ app/src/main/java/com/example/tactalk/StatsFragment.kt
@@ -0,0 +1,17 @@
+
+
+package com.example.tactalk
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class StatsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stats, container, false)
+    }
+}
